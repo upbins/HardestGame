d3c703f452943f5eeaf450d6b44d91e24e73a874
孟家彬 <mengjiabin@shiyuegame.com>
2018-11-10 16:20:29 +0800

    youhua

--- build/jsb-default/frameworks/runtime-src/proj.android-studio/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/cocos2dx_static/scripting/js-bindings/jswrapper/MappingUtils.o.d
+++
@@ -1,8 +0,0 @@
-/Users/upbins/CodeProject/CreatorProject/HardestGame/build/jsb-default/frameworks/runtime-src/proj.android-studio/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/cocos2dx_static/scripting/js-bindings/jswrapper/MappingUtils.o: \
- /Users/upbins/CodeProject/CreatorProject/HardestGame/build/jsb-default/frameworks/cocos2d-x/cocos/scripting/js-bindings/jswrapper/MappingUtils.cpp \
- /Users/upbins/CodeProject/CreatorProject/HardestGame/build/jsb-default/frameworks/cocos2d-x/cocos/scripting/js-bindings/jswrapper/MappingUtils.hpp \
- /Users/upbins/tools/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/unordered_map
-
-/Users/upbins/CodeProject/CreatorProject/HardestGame/build/jsb-default/frameworks/cocos2d-x/cocos/scripting/js-bindings/jswrapper/MappingUtils.hpp:
-
-/Users/upbins/tools/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/unordered_map:
